b12d9653b15daca5611d99737daeb8712655b71c
Diogo Nogal Santos <diogo.nogal.santos@gmail.comm>
2024-07-18 12:21:51 +0200

    CS-Kata 2 - LeapYear

--- 2_LeapYear/kata.cs
+++ 2_LeapYear/kata.cs
@@ -7,4 +7,35 @@ public class LeapYearShould
 {
     [Fact]
     public void DoSomething() => true.Should().BeTrue();
+
+    [Theory]
+    [InlineData(1, false)]
+    [InlineData(2, false)]
+    [InlineData(3, false)]
+    [InlineData(100, false)]
+    [InlineData(400, true)]
+
+    public void NotBeLeapYears(int input, bool expected)
+    {
+        var leapYear = new LeapYear();
+
+        bool result = leapYear.IsLeapYear(input);
+
+        result.Should().Be(expected);
+    }
+    public class LeapYear
+    {
+        public bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0)
+                return true;
+            if (year % 100 == 0)
+                return false;
+            if (year % 4 == 0)
+                return true;
+            return false;
+        }
+    }
 }
+
+
